98355d674e2a3f26f96eac297e79e477a2f14ec5
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2022-11-14 20:58:37 +0530

    Create Hierarchical_inheritance.cpp
    Hierarchical Inheritance

---
+++ Hierarchical_inheritance.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+	public:
+		A()
+		{
+			cout<<"Constructor of class A"<<endl;
+			
+		}
+};
+
+class B:public A
+{
+	public:
+		B()
+		{
+			cout<<"Constructor of class B"<<endl;
+			
+		}
+};
+
+class C:public A
+{
+	public:
+		C()
+		{
+			cout<<"Constructor of class C"<<endl;
+		}
+};
+
+int main()
+{
+	C obj;
+	return 0;
+}
+
