95880ee6ceb42925a5f3663682430e23f0d6f612
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2022-11-15 20:12:41 +0530

    Create changing_values.cpp
    Changing values pointed by pointers.

---
+++ changing_values.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int var=5;
+    int* pointvar;
+    
+    pointvar=&var;
+    
+    cout<<"var="<<var<<endl;
+    cout<<"*pointvar="<<*pointvar<<endl<<endl;
+    cout<<"Changing value of var to 7:"<<endl;
+    
+    var=7;
+    cout<<"var="<<var<<endl;
+    cout<<"*pointvar="<<*pointvar<<endl<<endl;
+    cout<<"Changing value of *pointvar to 16:"<<endl;
+    
+    *pointvar=16;
+    cout<<"var="<<var<<endl;
+    cout<<"*pointvar="<<*pointvar<<endl;
+    return 0;
+}
