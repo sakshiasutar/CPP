5cbc99b6b1784e8b7e3d6460167e5b87ce4012e5
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2022-11-28 20:48:50 +0530

    Create calloc.cpp
    Write a C++ program to find sum of n elements entered by user .To perform this program ,allocate memory dynamically using calloc() function.

---
+++ calloc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	int n,i,*ptr,sum=0;
+	cout<<"How many element:";
+	cin>>n;
+	ptr=(int*)calloc(n,sizeof(int));
+	if(ptr==NULL)
+	{
+		cout<<"Error in allocating memory";
+		exit(0);
+	}
+	
+	cout<<"Enter elements:";
+	for(i=0;i<n;++i)
+	{
+		cin>>*(ptr+1);
+		sum+=*(ptr+1);
+		
+	}
+	cout<<"Sum of elements:"<<sum;
+	free(ptr);
+	return 0;
+}
